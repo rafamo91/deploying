a68731ca35d239ed4f50a340da4457272f83498f
Rafa <rafa.m.o91@gmail.com>
2021-02-16 12:16:37 +0100

    commit

---
+++ deployJenkins/src/main/java/com/deploy/jenkins/DeployJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployJenkinsApplication.class, args);
+	}
+
+}
---
+++ deployJenkins/src/test/java/com/deploy/jenkins/DeployJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
